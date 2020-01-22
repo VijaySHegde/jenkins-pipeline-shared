5aa4ab7613af7446cecaee39bda4b7c3a0d72e6b
VijaySHegde <55663295+VijaySHegde@users.noreply.github.com>
2020-01-23 01:04:54 +0530

    Update confluence.groovy

--- vars/confluence.groovy
+++ vars/confluence.groovy
@@ -1,3 +1,7 @@
-curl -u admin:admin -X POST -H 'Content-Type: application/json' -d '{"type":"page","title":"page1",
-"space":{"key":"VIJ"},"body":{"storage":{"value":"<p>This is <br/> a new page</p>","representation":
+def call(def pagename, def spaceid)
+{
+  
+  curl -u admin:admin -X POST -H 'Content-Type: application/json' -d '{"type":"page","title":"${pagename}",
+  "space":{"key":"${spaceid}"},"body":{"storage":{"value":"<p>This is <br/> a new page</p>","representation":
 "storage"}}}' http://3.15.148.45:8090/confluence/rest/api/content/ | python -mjson.tool
+}
