d986215cc00747ad6e5e71e126c8a8ac65d6fd1b
VijaySHegde <55663295+VijaySHegde@users.noreply.github.com>
2020-01-23 01:21:07 +0530

    Update confluence.groovy

--- vars/confluence.groovy
+++ vars/confluence.groovy
@@ -1,7 +1,7 @@
 def call(def pagename, def spaceid)
 {
   
-  curl -u admin:admin -X POST -H 'Content-Type: application/json' -d '{"type":"page","title":"${pagename}",
+  curl -u ashnim:jira@123 -X POST -H 'Content-Type: application/json' -d '{"type":"page","title":"${pagename}",
   "space":{"key":"${spaceid}"},"body":{"storage":{"value":"<p>This is <br/> a new page</p>","representation":
 "storage"}}}' http://3.15.148.45:8090/confluence/rest/api/content/ | python -mjson.tool
 }
