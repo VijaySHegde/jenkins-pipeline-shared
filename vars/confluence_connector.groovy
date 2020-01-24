75b5a36eefbb59445f6415b8cb4b152ffcc96414
VijaySHegde <55663295+VijaySHegde@users.noreply.github.com>
2020-01-24 12:43:22 +0530

    Update and rename confluence.groovy to confluence_connector.groovy

---
+++ vars/confluence_connector.groovy
@@ -0,0 +1,12 @@
+def call(def keyname, def spacename)
+{
+  curl --request POST \
+  --url https://vijaysh.atlassian.net/wiki/rest/api/space \
+  --header 'authorization: Basic aGVnZGV2aWpheTExOEBnbWFpbC5jb206U3B4MEdVWGRLREhpVmVCRXdJMFhDRDU5' \
+  --header 'cache-control: no-cache' \
+  --header 'content-type: application/json' \
+  --header 'postman-token: 44321770-69d0-0b6b-9732-731230318d5b' \
+  --data '{\r\n    "key": "${keyname}",\r\n    "name": "${spacename}",\r\n    "description": {\r\n        "plain": {\r\n            "value": "This is an example space",\r\n            "representation": "plain"\r\n        }\r\n    },\r\n    "metadata": {}\r\n}\r\n'
+           
+
+}
