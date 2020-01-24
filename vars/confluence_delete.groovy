a7a78ea89d1b654f19254bc3a9a19726741a105a
VijaySHegde <55663295+VijaySHegde@users.noreply.github.com>
2020-01-24 18:35:39 +0530

    Update confluence_delete.groovy

--- vars/confluence_delete.groovy
+++ vars/confluence_delete.groovy
@@ -1,5 +1,5 @@
 def call()
 {
-            sh 'curl -v -S -u hegdevijay118@gmail.com:Spx0GUXdKDHiVeBEwI0XCD59 -X DELETE https://vijaysh.atlassian.net/wiki/rest/api/content/262617'
+            sh 'curl -v -S -u hegdevijay118@gmail.com:Spx0GUXdKDHiVeBEwI0XCD59 -X DELETE https://vijaysh.atlassian.net/wiki/rest/api/content/65672'
 
 }
