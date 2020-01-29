706a39c967e0f0a25e31334b878ff2b0576f8aeb
VijaySHegde <55663295+VijaySHegde@users.noreply.github.com>
2020-01-29 19:16:55 +0530

    Create confluenceConnectorSpace.groovy

---
+++ vars/confluenceConnectorSpace.groovy
@@ -0,0 +1,23 @@
+import groovy.json.JsonSlurper 
+
+@NonCPS
+createRepo(String data){
+def jsonSlurper = new JsonSlurper() 
+def resultJson = jsonSlurper.parseText(data)
+def spaceName = resultJson.name
+def keyName = resultJson.key
+//def projUrl = resultJson.url
+
+httpRequest authentication: 'confluence_cred', contentType: "APPLICATION_JSON", 
+    
+    httpMode: 'POST', requestBody: 
+  """{
+    	"name"=${spaceName}
+      "key"=${keyName}
+        
+   }""", url: "https://vijaysh.atlassian.net/wiki/rest/api/space"
+}
+	def call(){
+def request = libraryResource 'data.json'
+createRepo(request)
+}
