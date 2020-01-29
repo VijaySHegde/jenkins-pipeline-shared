f353a02c11b9e32b6520c9846b939f4d22d892f0
VijaySHegde <55663295+VijaySHegde@users.noreply.github.com>
2020-01-29 23:30:31 +0530

    Create confluence2.groovy

---
+++ vars/confluence2.groovy
@@ -0,0 +1,18 @@
+import groovy.json.JsonSlurper 
+
+@NonCPS
+showRepo(String data){
+def jsonSlurper = new JsonSlurper() 
+def resultJson = jsonSlurper.parseText(data)
+//def repoName = resultJson.name
+def spaceKey = resultJson.key
+//def projUrl = resultJson.url
+
+httpRequest authentication: 'confluence_cred', contentType: "APPLICATION_JSON", 
+    
+    httpMode: 'GET', url: "https://vijaysh.atlassian.net/wiki/rest/api/space/${spaceKey}"
+}
+	def call(){
+def request = libraryResource 'detail.json'
+showRepo(request)
+}
