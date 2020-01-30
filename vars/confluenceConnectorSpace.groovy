5bb993bb19c25701f3570f93eb8cc0faa9cbad0a
VijaySHegde <55663295+VijaySHegde@users.noreply.github.com>
2020-01-30 16:59:28 +0530

    Update confluenceConnectorSpace.groovy

--- vars/confluenceConnectorSpace.groovy
+++ vars/confluenceConnectorSpace.groovy
@@ -23,21 +23,6 @@ httpRequest authentication: 'confluence_cred',
 	def call(){
 def request = libraryResource 'data.json'
 createRepo(request)
-		for (item in Hudson.instance.items) {
-  println("\njob: $item.name")
-  hasTimestamper = false;
-  item.buildWrappersList.each {
-    if (it instanceof hudson.plugins.timestamper.TimestamperBuildWrapper) {
-      hasTimestamper = true;
-    }
-  }
-  if (!hasTimestamper) {
-    println(">>>>>>>> Adding timestamper right to $item.name")
-    item.buildWrappersList.add(new hudson.plugins.timestamper.TimestamperBuildWrapper());
-    item.save()
-  }
-}
-
 }
 
 
