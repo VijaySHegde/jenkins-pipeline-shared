c6c6f29cc4f4e061da342c4c804327ceb147d716
VijaySHegde <55663295+VijaySHegde@users.noreply.github.com>
2020-01-30 14:48:31 +0530

    Update confluenceConnectorSpace.groovy

--- vars/confluenceConnectorSpace.groovy
+++ vars/confluenceConnectorSpace.groovy
@@ -4,10 +4,10 @@ import groovy.json.JsonSlurper
 createRepo(String data1){
 def jsonSlurper = new JsonSlurper() 
 def resultJson = jsonSlurper.parseText(data1)
-def keyName = resultJson.key
-def spaceName = resultJson.name
+def keyName = '"'+resultJson.key+'"'
+def spaceName = '"'+resultJson.name+'"'
 //def projUrl = resultJson.url
-
+echo "$keyName"
 httpRequest authentication: 'confluence_cred', 
 	customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json'], 
                     [maskValue: false, name: 'Accept', value: 'application/json']],
