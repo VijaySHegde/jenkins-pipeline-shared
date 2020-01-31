b51269ec3c1eefbdc7545a5daf4385bf1fd1d5e3
VijaySHegde <55663295+VijaySHegde@users.noreply.github.com>
2020-01-31 15:59:06 +0530

    Update confluenceCollectorSpace.groovy

--- vars/confluenceCollectorSpace.groovy
+++ vars/confluenceCollectorSpace.groovy
@@ -10,7 +10,7 @@ def spaceKey = resultJson.key
 
 httpRequest authentication: 'confluence_cred', contentType: "APPLICATION_JSON", 
     
-    httpMode: 'GET', url: "https://vijaysh.atlassian.net/wiki/rest/api/space/${spaceKey}"
+    httpMode: 'GET', url: "https://vijaysh1.atlassian.net/wiki/rest/api/space/${spaceKey}"
 }
 	def call(){
 def request = libraryResource 'confluence1.json'
