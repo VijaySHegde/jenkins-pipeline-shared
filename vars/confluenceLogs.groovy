4534af6fa657277ff417f3d6702eda1403389e7c
VijaySHegde <55663295+VijaySHegde@users.noreply.github.com>
2020-01-31 11:56:01 +0530

    Create confluenceLogs.groovy

---
+++ vars/confluenceLogs.groovy
@@ -0,0 +1,12 @@
+import groovy.json.JsonSlurper 
+@NonCPS
+def call(message)
+{
+ println(message)
+ def request = libraryResource 'data1.json' //using library resource for fetching json file
+ def jsonSlurper = new JsonSlurper() //creating object for json slurper
+ def resultJson = jsonSlurper.parseText(request)// here we are retriving json file
+ def keyName = resultJson.key // your key/name used for rest api
+  Date date = new Date() // to print timestamp
+  sh " echo '${date}' confluence project with the keyName  '${keyName}' ${message} >>log.txt"
+}
