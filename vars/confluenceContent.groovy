463dfe3a3d5c6154493f1816ae263c21e9fbeffc
VijaySHegde <55663295+VijaySHegde@users.noreply.github.com>
2020-02-04 14:54:57 +0530

    Update confluenceContent.groovy

--- vars/confluenceContent.groovy
+++ vars/confluenceContent.groovy
@@ -1,18 +1,35 @@
 import groovy.json.JsonSlurper 
 
 @NonCPS
-showSpace(String data){
+createSpace(String data1){
 def jsonSlurper = new JsonSlurper() 
-def resultJson = jsonSlurper.parseText(data)
-//def repoName = resultJson.name
-def spaceKey = resultJson.key
+def resultJson = jsonSlurper.parseText(data1)
+def keyName = resultJson.key
+//def spaceName = resultJson.name
+def titleName = resultJson.title
+def typeName = resultJson.type
+def bodyName = resultJson.body
 //def projUrl = resultJson.url
-
-httpRequest authentication: 'confluence_cred', contentType: "APPLICATION_JSON", 
+echo "$keyName"
+httpRequest authentication: 'confluence_cred', 
+	customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json'], 
+                    [maskValue: false, name: 'Accept', value: 'application/json']],
     
-    httpMode: 'GET', url: "https://vijaysh1.atlassian.net/wiki/rest/api/space/${spaceKey}"
+    httpMode: 'POST', requestBody: 
+  """{
+    	
+      "key":"${keyName}",
+    
+      "title":"${titleName}",
+      "type":"${typeName}",
+      "body":"${bodyName}"
+      
+        
+   }""", url: "https://vijaysh1.atlassian.net/wiki/rest/api/content"
 }
 	def call(){
-def request = libraryResource 'confluence1.json'
-showSpace(request)
+def request = libraryResource 'confluence2.json'
+createSpace(request)
 }
+
+
