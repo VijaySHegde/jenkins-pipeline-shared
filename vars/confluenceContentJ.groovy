e419888669af94dda5d07fa49b58bcf9e7cb3a6a
VijaySHegde <55663295+VijaySHegde@users.noreply.github.com>
2020-02-05 18:50:37 +0530

    Create confluenceContentJ.groovy

---
+++ vars/confluenceContentJ.groovy
@@ -0,0 +1,31 @@
+def call(jsondata){
+def jsonString = jsondata
+//println(jsonString)
+def jsonObj = readJSON text: jsonString
+println(jsonObj.confluence)
+
+String a=jsonObj.confluence.spaces.space.name
+String projectName=a.replaceAll("\\[", "").replaceAll("\\]","");
+String b=jsonObj.confluence.spaces.space.key
+String keyName=b.replaceAll("\\[", "").replaceAll("\\]","");
+println(projectName)
+println(keyName)
+
+	
+//env.name = projectName
+
+//env.key = keyName
+  
+
+httpRequest authentication: 'confluence_cred1', 
+	customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json'], 
+                    [maskValue: false, name: 'Accept', value: 'application/json']],
+    
+    httpMode: 'POST', requestBody: 
+  """{
+    	
+      
+        "key":"${projectName}",
+        "name":"${keyName}"
+   }""",responseHandle: 'NONE', url: "http://ec2-3-15-148-45.us-east-2.compute.amazonaws.com:8090/rest/api/space"
+}
