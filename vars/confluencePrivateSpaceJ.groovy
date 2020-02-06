5033d7731f3c6fc4713c36596e2b9c67a638f2df
VijaySHegde <55663295+VijaySHegde@users.noreply.github.com>
2020-02-06 14:46:06 +0530

    Create confluencePrivateSpaceJ.groovy

---
+++ vars/confluencePrivateSpaceJ.groovy
@@ -0,0 +1,45 @@
+def call(jsondata){
+def jsonString = jsondata
+//println(jsonString)
+def jsonObj = readJSON text: jsonString
+println(jsonObj.confluence)
+
+String a=jsonObj.confluence.spaces.space.key
+String keyName=a.replaceAll("\\[", "").replaceAll("\\]","");
+String b=jsonObj.confluence.spaces.space.pages.page.title
+String titleName=b.replaceAll("\\[", "").replaceAll("\\]","");
+String c=jsonObj.confluence.spaces.space.pages.page.type
+String typeName=c.replaceAll("\\[", "").replaceAll("\\]","");
+
+	
+
+println(keyName)
+println(titleName)
+println(typeName)
+
+	
+//env.name = projectName
+
+//env.key = keyName
+  
+
+httpRequest authentication: 'confluence_cred1', 
+	customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json'], 
+                    [maskValue: false, name: 'Accept', value: 'application/json']],
+    
+    httpMode: 'POST', requestBody: 
+  """{
+    	
+   
+   	"space":{
+		"key":"${keyName}"
+	},
+	
+	"title": "${titleName}",
+	 "type": "${typeName}"
+	
+	}
+	
+   }""", url: 'http://ec2-3-15-148-45.us-east-2.compute.amazonaws.com:8090/rest/api/content'
+	//,validResponseCodes: '200:600' for resolving not in range error.
+}
