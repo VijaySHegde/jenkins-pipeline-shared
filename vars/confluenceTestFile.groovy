8b8169c827bb825268902fa02d9c9ffea6cf33ba
VijaySHegde <55663295+VijaySHegde@users.noreply.github.com>
2020-02-07 00:44:24 +0530

    Create confluenceTestFile.groovy

---
+++ vars/confluenceTestFile.groovy
@@ -0,0 +1,15 @@
+Map modules = [:]
+pipeline {
+    agent any
+    stages {
+        stage('test') {
+            steps {
+                script{
+                    modules.confluenceTest = load "confluenceTest.groovy"
+                    modules.confluenceTest.call()
+                    modules.confluenceTest.info()
+                }
+            }
+        }
+    }
+}
