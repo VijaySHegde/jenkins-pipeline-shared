d537f84b613ff0c144f62a8114ff2acc69642191
VijaySHegde <55663295+VijaySHegde@users.noreply.github.com>
2020-02-06 23:03:09 +0530

    Create confluenceDeleteContentJ.groovy

---
+++ vars/confluenceDeleteContentJ.groovy
@@ -0,0 +1,32 @@
+def call(jsondata){
+def jsonString = jsondata
+//println(jsonString)
+def jsonObj = readJSON text: jsonString
+println(jsonObj.confluence)
+
+//String a=jsonObj.confluence.spaces.space.
+//String keyName=a.replaceAll("\\[", "").replaceAll("\\]","");
+String b=jsonObj.confluence.spaces.space.pages.page.id
+String pageId=b.replaceAll("\\[", "").replaceAll("\\]","");
+//String c=jsonObj.confluence.spaces.space.pages.page.type
+//String typeName=c.replaceAll("\\[", "").replaceAll("\\]","");
+
+	
+
+println(pageId)
+//println(privateName)
+
+
+	
+//env.name = projectName
+
+//env.key = keyName
+  
+
+httpRequest authentication: 'confluence_cred1', 
+	customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json'], 
+                    [maskValue: false, name: 'Accept', value: 'application/json']],
+    
+    httpMode: 'DELETE', url: "http://ec2-3-15-148-45.us-east-2.compute.amazonaws.com:8090/rest/api/space/${pageId}
+	//,validResponseCodes: '200:600' for resolving not in range error.
+}
