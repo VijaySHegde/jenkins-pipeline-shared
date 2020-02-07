dd3bd34c8b45a40c687b5e4f51dc19b212e5d711
VijaySHegde <55663295+VijaySHegde@users.noreply.github.com>
2020-02-07 13:25:44 +0530

    Update confluenceGroupJ.groovy

--- vars/confluenceGroupJ.groovy
+++ vars/confluenceGroupJ.groovy
@@ -22,11 +22,26 @@ println(groupName)
 
 //env.key = keyName
   
+sh """ 
+curl -X GET \
+  http://ec2-3-15-148-45.us-east-2.compute.amazonaws.com:8090/rest/api/group/${name} \
+  -H 'authorization: Basic YXNobmltOmppcmFAMTIz' \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: application/json' \
+  -H 'postman-token: 3a14aa21-0edf-bf0a-2c60-4ba2d495e2e3' \
+  -d '{ "space":{
+	
 
-httpRequest authentication: 'confluence_cred1', 
+		"key":"ramya"
+},	
+	
+	"title": "ramya",
+	 "type": "page"
+}' """
+/*httpRequest authentication: 'confluence_cred1', 
 	customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json'], 
                     [maskValue: false, name: 'Accept', value: 'application/json']],
     
     httpMode: 'GET', url: "http://ec2-3-15-148-45.us-east-2.compute.amazonaws.com:8090/rest/api/group/${groupName}"
-	//,validResponseCodes: '200:600' for resolving not in range error.
+	//,validResponseCodes: '200:600' for resolving not in range error. */
 }
