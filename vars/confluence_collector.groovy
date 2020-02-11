255c39726cc4d5d7c3523db31680c04ad4115b18
VijaySHegde <55663295+VijaySHegde@users.noreply.github.com>
2020-02-11 19:38:39 +0530

    Update confluence_collector.groovy

--- vars/confluence_collector.groovy
+++ vars/confluence_collector.groovy
@@ -1,2 +1,4 @@
-def json = "http://ec2-3-15-148-45.us-east-2.compute.amazonaws.com:8090/rest/api/search?cql=space%3Dvijay%20AND%20type%3Dpage".toURL() getText(requestProperties: [Accept: 'application/json',
+def call () {
+    json1 = "http://ec2-3-15-148-45.us-east-2.compute.amazonaws.com:8090/rest/api/search?cql=space%3Dvijay%20AND%20type%3Dpage".toURL() getText(requestProperties: [Accept: 'application/json',
                             Authorization: 'Basic YXNobmltOmppcmFAMTIz'])
+}
