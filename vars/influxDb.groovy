5ffa1d2584b4367251043306cd015f959207ad8f
VijaySHegde <55663295+VijaySHegde@users.noreply.github.com>
2020-02-11 20:19:17 +0530

    Create influxDb.groovy

---
+++ vars/influxDb.groovy
@@ -0,0 +1,18 @@
+import groovy.json.*
+
+@NonCPS
+create(){
+def jsonSlurper = new JsonSlurper()
+def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/ouput.json"),"UTF-8"))
+def resultJson = jsonSlurper.parse(reader)
+def total = resultJson.totalSize
+  echo "$total"
+sh """curl -i -XPOST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' --data-binary 'confluencepages=${total}' 
+"""
+}
+
+
+def call()
+{
+create()
+}
