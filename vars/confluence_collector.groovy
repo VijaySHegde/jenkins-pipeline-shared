7b5cf4739ffe0343de44a2b35c3167d451ef574e
VijaySHegde <55663295+VijaySHegde@users.noreply.github.com>
2020-02-11 18:15:43 +0530

    Update confluence_collector.groovy

--- vars/confluence_collector.groovy
+++ vars/confluence_collector.groovy
@@ -6,6 +6,6 @@ A_PASSWD="jira@123"
 INSTANCE_HOST="http://ec2-3-15-148-45.us-east-2.compute.amazonaws.com:8090"
  
 # Total number of pages
-curl -sGLu ${A_USER}:${A_PASSWD} "https://${INSTANCE_HOST}/rest/api/search" --data-urlencode 'cql=type IN (blogpost, page)' --data-urlencode 'limit=0'| jq '.totalSize'
+curl -sGLu ${A_USER}:${A_PASSWD} "http://${INSTANCE_HOST}/rest/api/search" --data-urlencode 'cql=type IN (blogpost, page)' --data-urlencode 'limit=0'
  '''
 }
