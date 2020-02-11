44483569b382c646a621e0b66c5dd51298aeb9ba
VijaySHegde <55663295+VijaySHegde@users.noreply.github.com>
2020-02-11 18:41:05 +0530

    Update confluence_collector.groovy

--- vars/confluence_collector.groovy
+++ vars/confluence_collector.groovy
@@ -1,6 +1,10 @@
 def call()
 {
-sh ''' 
-curl -sGLu ashnim:jira@123 "http://ec2-3-15-148-45.us-east-2.compute.amazonaws.com:8090/rest/api/search" --data-urlencode 'cql=type IN (page)' --data-urlencode 'limit=0'
-'''
+sh '''	curl -X GET \
+  'http://ec2-3-15-148-45.us-east-2.compute.amazonaws.com:8090/rest/api/search?cql=space%3Dvijay%20AND%20type%3Dpage' \
+  -H 'authorization: Basic YXNobmltOmppcmFAMTIz' \
+  -H 'cache-control: no-cache' \
+  -H 'postman-token: a13d0cc1-e2ff-c62c-8e77-483f2f8bd0d4' \
+  -H 'start: 1'
+  '''
 }
