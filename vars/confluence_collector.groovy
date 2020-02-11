62bf32592bdc819ebbf9da15cf8958ada059a6f6
VijaySHegde <55663295+VijaySHegde@users.noreply.github.com>
2020-02-11 18:09:25 +0530

    Update confluence_collector.groovy

--- vars/confluence_collector.groovy
+++ vars/confluence_collector.groovy
@@ -1,12 +1,10 @@
 def call()
 {
 sh ''' 
-curl --request GET \
-  --url https://vijaysh.atlassian.net/wiki/rest/api/space \
-  --header 'authorization: Basic aGVnZGV2aWpheTExOEBnbWFpbC5jb206U3B4MEdVWGRLREhpVmVCRXdJMFhDRDU5' \
-  --header 'cache-control: no-cache' \
-  --header 'content-type: application/json' \
-  --header 'postman-token: 7a3d7cbd-3984-036a-7915-dbbab83a8245' \
-  --data '{\r\n    "key": "TEST1",\r\n    "name": "Example space",\r\n    "description": {\r\n        "plain": {\r\n            "value": "This is an example space",\r\n            "representation": "plain"\r\n        }\r\n    },\r\n    "metadata": {}\r\n}\r\n'
-  '''
+A_USER="ashnim"
+A_PASSWD="jira@123"
+INSTANCE_HOST="http://ec2-3-15-148-45.us-east-2.compute.amazonaws.com:8090"
+ 
+# Total number of pages
+curl -sGLu ${A_USER}:${A_PASSWD} "https://${INSTANCE_HOST}/rest/api/search" --data-urlencode 'cql=type IN (blogpost, page)' --data-urlencode 'limit=0'
   }
