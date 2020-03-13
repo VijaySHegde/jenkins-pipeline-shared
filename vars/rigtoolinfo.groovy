e2e94d563c7730e1bb2ca70bed150b2fd0c01d33
REVATHISARAISWATHI <revathisaraiswathi@gmail.com>
2020-03-13 12:25:05 +0530

    Update rigtoolinfo.groovy

--- vars/rigtoolinfo.groovy
+++ vars/rigtoolinfo.groovy
@@ -4,7 +4,7 @@ import groovy.json.JsonOutput
 def call()
 {
    
- sh "curl -X POST  -H  Accept: application/json -H  Content-Type: application/json -d @resources/rig1.json  http://3.134.156.211:3013/api/riglets/connectorServerDetails -o rigoutput.json"
+ sh "curl -X POST  -H  Accept: application/json -H  Content-Type: application/json -d @'./resources/rig1.json'  http://3.134.156.211:3013/api/riglets/connectorServerDetails -o rigoutput.json"
    
 def jsonSlurper = new JsonSlurper()
 def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/rigoutput.json"),"UTF-8"))
