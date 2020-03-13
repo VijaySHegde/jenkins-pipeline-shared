69fe8dfe67734a85c23307c148876de97ee44e52
younu8084 <younu8084@gmail.com>
2020-03-13 13:38:57 +0530

    Update rig.groovy

--- vars/rig.groovy
+++ vars/rig.groovy
@@ -6,16 +6,10 @@ def jsonSlurper = new JsonSlurper()
 def resultJson = jsonSlurper.parseText(data)
 def rigletName = resultJson.rigletName
 def toolName= resultJson.toolName
-
- 
-
-    
- requestBody:
+ requestBody
   {
-     "rigletName":"${rigletName}",
+    "rigletName":"${rigletName}",
     "toolName":"${toolName}"
-
-
   }
   sh "curl -X POST -H  Accept: application/json -H  Content-Type: application/json -d @requestBody  'http://3.134.156.211:3013/api/riglets/connectorServerDetails' -o rigoutput.json"
     /* def connection = url.toURL().openConnection()
