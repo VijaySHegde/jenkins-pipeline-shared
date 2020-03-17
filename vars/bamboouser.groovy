9d071a66d5722b41c2ca2989d8ca6a82a87db833
REVATHISARAISWATHI <revathisaraiswathi@gmail.com>
2020-03-17 17:15:09 +0530

    Update bamboouser.groovy

--- vars/bamboouser.groovy
+++ vars/bamboouser.groovy
@@ -20,7 +20,8 @@ def key= jsonObj.ci.projectplankey.key
 String sresponse = sh(script: """curl  -X GET -L -w '%{url_effective} %{http_code}\\n' -u ${username}:${password} '${IP}/rest/api/latest/result/${key}.json?max-result=50&expand=results.result.artifacts&expand=changes.change.files&start-index=0'  """, returnStdout: true)
 println("hi")
 	//println(sresponse) 
-	String res=sresponse.split('200')
+	def r= readJSON text: sresponse
+	String res=r.split('200')
 	println(res[1])
 	println("hi")
 	println(res[2])
