1537e9aad55aac3617dabca58c8d9980a1f0f08b
REVATHISARAISWATHI <revathisaraiswathi@gmail.com>
2020-03-17 12:30:57 +0530

    Update bamboouser.groovy

--- vars/bamboouser.groovy
+++ vars/bamboouser.groovy
@@ -21,6 +21,7 @@ String sresponse = sh(script: """curl  -X GET  -v -u ${username}:${password} '${
 //println(sresponse) 
 	String res=sresponse.split('* Connection #0 to host ec2-18-220-143-53.us-east-2.compute.amazonaws.com left intact')
 	println(res[0])
+	println("hi")
 	println(res[1])
 	String response = sh(script: """curl  -X GET  -u ${username}:${password} '${IP}/rest/api/latest/result/${key}.json?max-result=50&expand=results.result.artifacts&expand=changes.change.files&start-index=0'  """, returnStdout: true)
 //println(response) 
