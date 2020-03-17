ad79e1f4502e4442e3e4d29f3d3c3a14e10e1785
REVATHISARAISWATHI <revathisaraiswathi@gmail.com>
2020-03-17 11:27:04 +0530

    Update bamboouser.groovy

--- vars/bamboouser.groovy
+++ vars/bamboouser.groovy
@@ -18,7 +18,7 @@ def key= jsonObj.ci.projectplankey.key
 //println(key)
  //withCredentials([usernamePassword(credentialsId: 'bamboo_cred', passwordVariable: 'password', usernameVariable:'username')]) {
 String response = sh(script: """curl  -X GET  -u ${username}:${password} '${IP}/rest/api/latest/result/${key}.json?max-result=50&expand=results.result.artifacts&expand=changes.change.files&start-index=0' """, returnStdout: true)
-println(response)  
+//println(response)  
 	//-o outputbamboo.json
 
 // }
