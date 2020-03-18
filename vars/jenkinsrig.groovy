59344e0973294bee8edbb83ca4e961fb55f855b5
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2020-03-18 20:03:49 +0530

    Update jenkinsrig.groovy

--- vars/jenkinsrig.groovy
+++ vars/jenkinsrig.groovy
@@ -14,7 +14,7 @@ def mailcount = jsonObj.riglet_info.auth_users.size()
 	print(mailcount)
 //withCredentials([usernamePassword(credentialsId: 'jenkins_cred', passwordVariable: 'pass', usernameVariable: 'user')]) {
 //sh "curl -X GET -g http://52.14.229.175:8080/job/${JOB_NAME}/api/json?tree=builds[id,result,changeSets[items[authorEmail]]] -u suneel:11035ac86f58bc32d03d8e873b7cc063a3 -o username.json"
-	sh "curl -X GET -g http://52.15.231.47:8080/job/${ProjectName}/api/json?tree=builds[id,result,changeSets[items[authorEmail]]] -u admin:1129a01d848ade5671280410cd55dcb376 -o username.json"
+	sh "curl -X GET -g http://3.130.89.18:8080/job/${ProjectName}/api/json?tree=builds[id,result,changeSets[items[authorEmail]]] -u admin:1129a01d848ade5671280410cd55dcb376 -o username.json"
 	//}
 	def jsonSlurper = new JsonSlurper()
 def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/username.json"),"UTF-8"))
