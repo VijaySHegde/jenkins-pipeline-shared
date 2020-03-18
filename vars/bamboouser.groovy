798fdb3b80e5d930e97022251c1d6b74c93b7772
REVATHISARAISWATHI <revathisaraiswathi@gmail.com>
2020-03-18 14:28:14 +0530

    Update bamboouser.groovy

--- vars/bamboouser.groovy
+++ vars/bamboouser.groovy
@@ -156,10 +156,11 @@ def resultJson = jsonSlurper.parse(reader)
 	//println(jsonBuilder)
 //jsonBuilder =jsonBuilder.Stringify()
 	return jsonBuilder
+		if(response==404)
+	throw new Exception ("Not found");
 
 }
-	if(response==404)
-	throw new Exception ("Not found");
+	
 catch(Exception e)
 {
 	println(response)
